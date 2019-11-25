ee37696e78c957a90683a42a4d7f6660bacbe35f
jvo203 <chris.zapart@nao.ac.jp>
2019-11-25 10:31:32 +0900

    modified:   server.py

--- server.py
+++ server.py
@@ -1,15 +1,20 @@
 from http.server import BaseHTTPRequestHandler
+
 class Server(BaseHTTPRequestHandler):
-    def __init__(self, root):
-        # body of the constructor
-        self.htdocs = root
+    def __init__(self, htdocs):
+        self.htdocs = htdocs
+        
     def do_HEAD(self):
         return
+    
     def do_POST(self):
         return
+    
     def do_GET(self):
         return
+    
     def handle_http(self):
         return
+    
     def respond(self):
         return
