7a05665bf146a6a6485d385b8ff223503d7f4f23
jvo203 <chris.zapart@nao.ac.jp>
2019-11-25 16:45:58 +0900

    modified:   server.py

--- server.py
+++ server.py
@@ -3,6 +3,20 @@ from pathlib import Path
 import os
 from urllib import parse
 
-class Server(SimpleHTTPRequestHandler):    
+
+class Server(SimpleHTTPRequestHandler):
     def do_GET(self):
-        super().do_GET()
+        self.directory = "htdocs"
+
+        if "GAIAWebQL.html" in self.path:
+            pos = self.path.find('?')
+            if pos > 0:
+                params = parse.parse_qs(self.path[(pos+1):])
+                print(params)
+                self.send_error(501)                
+                #self.send_response(200)
+                #self.send_header('Content-type', "text/plain")
+                #self.end_headers()
+                #self.wfile.write(bytes("GAIAWebQL", "UTF-8"))
+        else:
+            super().do_GET()
