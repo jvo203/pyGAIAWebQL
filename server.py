e2de8ec05e0a1dbd757efc234e5fdcf39eb85fa6
jvo203 <chris.zapart@nao.ac.jp>
2019-11-27 14:06:44 +0900

    modified:   server.py

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from http.server import SimpleHTTPRequestHandler
 from pathlib import Path
-import os, io
+import os
+import io
 from urllib import parse
 
 
@@ -14,17 +15,37 @@ class Server(SimpleHTTPRequestHandler):
             if pos > 0:
                 params = parse.parse_qs(self.path[(pos+1):])
                 print(params)
-                #self.send_error(501)
-                
+                # self.send_error(501)
+
                 self.send_response(200)
                 self.send_header('Content-type', "text/html")
                 self.end_headers()
 
                 html = io.StringIO()
-                html.write("<!DOCTYPE html>\n<html>\n<head>\n<meta charset=\"utf-8\">\n")
-                html.write("<link href=\"https://fonts.googleapis.com/css?family=Inconsolata\" rel=\"stylesheet\"/>\n")
-                html.write("<link href=\"https://fonts.googleapis.com/css?family=Material+Icons\" rel=\"stylesheet\"/>\n")
-                html.write("<script src=\"reconnecting-websocket.js\" defer></script>\n")
+                html.write(
+                    "<!DOCTYPE html>\n<html>\n<head>\n<meta charset=\"utf-8\">\n")
+                html.write(
+                    "<link href=\"https://fonts.googleapis.com/css?family=Inconsolata\" rel=\"stylesheet\"/>\n")
+                html.write(
+                    "<link href=\"https://fonts.googleapis.com/css?family=Material+Icons\" rel=\"stylesheet\"/>\n")
+                html.write(
+                    "<script src=\"reconnecting-websocket.js\" defer></script>\n")
+
+                # bootstrap
+                html.write(
+                    "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no, minimum-scale=1, maximum-scale=1\">\n")
+                html.write(
+                    "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\">\n")
+                html.write(
+                    "<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js\"></script>\n")
+                html.write(
+                    "<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\"></script>\n")
+
+                html.write("<script>var WS_SOCKET = 'ws://';</script>")
+
+                html.write("<title>GAIA DR2 WebQL</title></head><body>\n")
+                html.write("<h1>GAIA DR2 WebQL</h1>")
+                html.write("</body></html>")
 
                 self.wfile.write(bytes(html.getvalue(), "UTF-8"))
         else:
