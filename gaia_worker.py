6897d95b31111233ca5e2dc97e9489d2c1b0cd2f
jvo203 <chris.zapart@nao.ac.jp>
2019-11-27 15:00:46 +0900

    modified:   gaia_worker.py

--- gaia_worker.py
+++ gaia_worker.py
@@ -1,6 +1,12 @@
-def execute_gaia(params, sessionid):    
-    """thread worker function"""    
+import multiprocessing
+
+
+def execute_gaia(params, sessionid):
+    """thread worker function"""
     print('Session ID: ', sessionid, '-->', params)
-    
+
     if 'xmin' in params:
         print(params['xmin'])
+
+    # make a shared queue
+    # pass the queue and params to #<num_procs> search workers (processes)
