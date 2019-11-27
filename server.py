657fe0461cf308cbaf31f86818149ca38b6209fa
jvo203 <chris.zapart@nao.ac.jp>
2019-11-27 13:49:09 +0900

    modified:   server.py

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from http.server import SimpleHTTPRequestHandler
 from pathlib import Path
-import os
+import os, io
 from urllib import parse
 
 
@@ -14,10 +14,18 @@ class Server(SimpleHTTPRequestHandler):
             if pos > 0:
                 params = parse.parse_qs(self.path[(pos+1):])
                 print(params)
-                self.send_error(501)
-                # self.send_response(200)
-                #self.send_header('Content-type', "text/plain")
-                # self.end_headers()
-                #self.wfile.write(bytes("GAIAWebQL", "UTF-8"))
+                #self.send_error(501)
+                
+                self.send_response(200)
+                self.send_header('Content-type', "text/html")
+                self.end_headers()
+
+                html = io.StringIO()
+                html.write("<!DOCTYPE html>\n<html>\n<head>\n<meta charset=\"utf-8\">\n")
+                html.write("<link href=\"https://fonts.googleapis.com/css?family=Inconsolata\" rel=\"stylesheet\"/>\n")
+                html.write("<link href=\"https://fonts.googleapis.com/css?family=Material+Icons\" rel=\"stylesheet\"/>\n")
+                html.write("<script src=\"reconnecting-websocket.js\" defer></script>\n")
+
+                self.wfile.write(bytes(html.getvalue(), "UTF-8"))
         else:
             super().do_GET()
