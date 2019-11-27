7a1e90d74fbd5875b86bfbc9afc772ad5ec9f38d
jvo203 <chris.zapart@nao.ac.jp>
2019-11-27 15:42:07 +0900

    modified:   gaia_worker.py

--- gaia_worker.py
+++ gaia_worker.py
@@ -1,4 +1,5 @@
 import multiprocessing
+import csv
 
 
 def execute_gaia(params, sessionid):
@@ -10,6 +11,13 @@ def execute_gaia(params, sessionid):
 
     num_procs = multiprocessing.cpu_count()
     print("launching", num_procs, "parallel search_gaia_db processes...")
+
+    with open('gaiadr2-table.dat') as csvfile:
+        dbCSV = csv.reader(csvfile, delimiter='|')
+        entries = []
+        for entry in dbCSV:
+            entries.append(entry)
+        print("PostgreSQL cluster database #entries:", len(entries))
 
     # make a shared queue
     # pass the queue and params to #<num_procs> search workers (processes)
