77aaaab2d472b291a46e4aeafd32d56eb2abee76
jvo203 <chris.zapart@nao.ac.jp>
2019-11-28 10:00:31 +0900

    modified:   server.py

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ from urllib import parse
 
 import multiprocessing
 import gaia_worker
+import json
 import uuid
 
 
@@ -23,6 +24,8 @@ class Server(SimpleHTTPRequestHandler):
                     self.send_error(501)
                     return
 
+                json.dump(params)                
+
                 id = uuid.uuid4()
                 search = multiprocessing.Process(
                     target=gaia_worker.execute_gaia, args=(params, id))
@@ -32,36 +35,34 @@ class Server(SimpleHTTPRequestHandler):
                 self.send_header('Content-type', "text/html")
                 self.end_headers()
 
-                html = io.StringIO()
-                html.write(
-                    "<!DOCTYPE html>\n<html>\n<head>\n<meta charset=\"utf-8\">\n")
-                html.write(
-                    "<link href=\"https://fonts.googleapis.com/css?family=Inconsolata\" rel=\"stylesheet\"/>\n")
-                html.write(
-                    "<link href=\"https://fonts.googleapis.com/css?family=Material+Icons\" rel=\"stylesheet\"/>\n")
-                html.write(
-                    "<script src=\"reconnecting-websocket.js\" defer></script>\n")
+                #html = io.StringIO()
+                #html.write(
+                #    "<!DOCTYPE html>\n<html>\n<head>\n<meta charset=\"utf-8\">\n")
+                #html.write(
+                #    "<link href=\"https://fonts.googleapis.com/css?family=Inconsolata\" rel=\"stylesheet\"/>\n")
+                #html.write(
+                #    "<link href=\"https://fonts.googleapis.com/css?family=Material+Icons\" rel=\"stylesheet\"/>\n")
+                #html.write(
+                #    "<script src=\"reconnecting-websocket.js\" defer></script>\n")
 
                 # bootstrap
-                html.write(
-                    "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no, minimum-scale=1, maximum-scale=1\">\n")
-                html.write(
-                    "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\">\n")
-                html.write(
-                    "<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js\"></script>\n")
-                html.write(
-                    "<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\"></script>\n")
+                #html.write(
+                #    "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no, minimum-scale=1, maximum-scale=1\">\n")
+                #html.write(
+                #    "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\">\n")
+                #html.write(
+                #    "<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js\"></script>\n")
+                #html.write(
+                #    "<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\"></script>\n")
 
-                html.write("<script>var WS_SOCKET = 'ws://';</script>")
+                #html.write("<script>var WS_SOCKET = 'ws://';</script>")
 
-                html.write("<title>GAIA DR2 WebQL</title></head><body>\n")
-                html.write(
-                    "<div id='session-data' style='width: 0; height: 0;' ")
-                html.write("data-uuid='" + str(id) + "'></div>\n")
-                html.write("<h1>GAIA DR2 WebQL</h1>")
-                html.write("</body></html>")
-
-                self.wfile.write(bytes(html.getvalue(), "UTF-8"))
-                html.close()
+                #html.write("<title>GAIA DR2 WebQL</title></head><body>\n")
+                #html.write(
+                #    "<div id='session-data' style='width: 0; height: 0;' ")
+                #html.write("data-uuid='" + str(id) + "'></div>\n")
+                #html.write("<h1>GAIA DR2 WebQL</h1>")
+                #html.write("</body></html>")
+                #self.wfile.write(bytes(html.getvalue(), "UTF-8"))                
         else:
             super().do_GET()
